225396aaa7ceee9f6f7dfe7a27faf55aed4e2e53
Vickiran <VickirS124@gmail.com>
2025-07-21 14:03:11 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include <QWidget>
 #include <QPushButton>
 #include <QLabel>
+#include <QProgressBar>
 #include <QVBoxLayout>
 #include <QThread>
 #include <QDebug>
 
-// Worker Thread
+// Worker thread that emits progress updates
 class Worker : public QThread
 {
     Q_OBJECT
@@ -17,54 +18,60 @@ signals:
 protected:
     void run() override {
         for (int i = 1; i <= 100; ++i) {
-            QThread::sleep(1); // Simulate work by sleeping
-            emit progress(i); // Emit the progress signal
+            QThread::sleep(1);          // Simulate work
+            emit progress(i);           // Notify UI thread
         }
     }
 };
 
-// Main Window
+// Main GUI Window
 class MainWindow : public QWidget
 {
     Q_OBJECT
 
 public:
     MainWindow() {
-        label = new QLabel("Click start to run the task", this);
+        label = new QLabel("Click 'Start Task' to begin", this);
+        progressBar = new QProgressBar(this);
+        progressBar->setRange(0, 100);
         button = new QPushButton("Start Task", this);
 
         QVBoxLayout* layout = new QVBoxLayout(this);
         layout->addWidget(label);
+        layout->addWidget(progressBar);
         layout->addWidget(button);
 
         setLayout(layout);
-        setWindowTitle("QThread Example");
-        setGeometry(300, 300, 300, 200);
+        setWindowTitle("Qt Job Progress Tracker");
+        setGeometry(300, 300, 350, 180);
 
         connect(button, &QPushButton::clicked, this, &MainWindow::startTask);
     }
 
 private slots:
     void startTask() {
+        // Prevent starting multiple threads
+        if (worker && worker->isRunning()) return;
+
         worker = new Worker();
-        connect(worker, &Worker::progress, this, &MainWindow::updateLabel);
+        connect(worker, &Worker::progress, this, &MainWindow::updateProgress);
+        connect(worker, &QThread::finished, worker, &QObject::deleteLater);
         worker->start();
     }
 
-    void updateLabel(int value) {
+    void updateProgress(int value) {
+        progressBar->setValue(value);
         label->setText(QString("Task Progress: %1%").arg(value));
         if (value == 100) {
             label->setText("Task Completed!");
-            worker->quit(); // Stop the worker thread
-            worker->wait(); // Ensure thread has finished
-            delete worker; // Clean up
         }
     }
 
 private:
     QLabel* label;
     QPushButton* button;
-    Worker* worker;
+    QProgressBar* progressBar;
+    Worker* worker = nullptr;
 };
 
 #include "main.moc"
